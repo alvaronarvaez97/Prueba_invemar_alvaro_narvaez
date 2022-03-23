942a928106da41788a6332ab8c4da1a2ad92da2b
Alvaro Narvaez <alvaronarvaez97@gmail.com>
2022-03-23 12:00:25 -0500

    actualizacion server.js con la conexion a base de datos utilizando Mysql y creacion de nuevo archivo router para hacer las peticiones y conexion a tablas especificas en la base de datos con sus respectivas rutas

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+const routes = require("./router");
 
 const app = express();
 
 app.set('port', process.env.PORT || 9003);
 
+app.use(express.json());
+//conexion base de datos-------------------
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',   
+    database: 'db_avistamiento_marino'
+}
+
+// middlewares -------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'));
+app.use(express.json());
+
+app.use('/api/resources', routes);
+
+//server
 app.listen(app.get('port'), ()=>{
     console.log('El servidor esta desplegado en el puerto ', app.get('port'));
   });
